acf5a7fc6d236649517b8a5b82672321d8a47379
WeirtyLagomme <gomezmickael@orange.fr>
2019-06-01 21:40:43 +0200

    Quick logs function reformating

--- cmds/logs.sh
+++ cmds/logs.sh
@@ -4,27 +4,27 @@
 function logs () {
     # Set locals
     local username="$1"
-    # Display logs
+    # Get logs
     local content=$(sed -e $'s/^/ /g' < "./usrs/$username/logs")
     local content_length=$(wc -l <<< "$content")
+    # Display header
     echo -e "\n [${CY}Logs of $username${NC}]"
-    if (( $content_length > 15 )); then
-        local line=1
-        while (( $line < $content_length )); do
-            local limit=$((line + 15))
-            echo -e "\n ${DI}# Line $line to $limit${NC} \n"
-            echo -e "$(sed $line,$limit!d <<< "$content")\n"
-            echo -e " > Press ${OR}q${NC} to ${OR}exit${NC} or ${BL}n${NC} to ${BL}continue${NC}"
-            while read -n1 -s log_action; do
-                [[ $log_action == "q" ]] && return 0
-                [[ $log_action != "n" ]] && continue
-                line=$limit
-                break
-            done
+    # Display small logs files
+    (( $content_length <= 15 )) && echo -e "\n$content" && return 0
+    # Split huge logs files
+    local line=1
+    while (( $line < $content_length )); do
+        local limit=$((line + 15))
+        echo -e "\n ${DI}# Line $line to $limit${NC} \n"
+        echo -e "$(sed $line,$limit!d <<< "$content")\n"
+        echo -e " > Press ${OR}q${NC} to ${OR}exit${NC} or ${BL}n${NC} to ${BL}continue${NC}"
+        while read -n1 -s log_action; do
+            [[ $log_action == "q" ]] && return 0
+            [[ $log_action != "n" ]] && continue
+            line=$limit
+            break
         done
-    else
-        echo -e "\n$content"
-    fi
+    done
 }
 
 function helpLogs () {
